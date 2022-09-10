70a284e010e3bf1c4cd383857a59dbbf889c8013
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-10 11:38:55 -0300

    feat: disable cursor when option is selected

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -11,11 +11,11 @@ const Option = ({ option, selectOption, answer }) => {
         border-2 border-purple-300 max-w-sm mx-auto my-4 rounded-lg px-4 py-2 mb-2 cursor-pointer hover:bg-purple-400 
       ${
         quizState.answerSelected && option === answer
-          ? 'bg-purple-700 hover:bg-purple-700'
+          ? 'bg-purple-700 hover:bg-purple-700 cursor-not-allowed'
           : ''
       } ${
         quizState.answerSelected && option !== answer
-          ? 'bg-red-900 hover:bg-red-900'
+          ? 'bg-red-900 hover:bg-red-900 cursor-not-allowed'
           : ''
       }
       `}
