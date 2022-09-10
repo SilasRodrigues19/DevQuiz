422f63ee92c761d62d74bb6bd8277be7ddb23ea3
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-09 21:07:12 -0300

    feat: add dynamic classes
    - Dynamic classes which will be displayed for error correct and wrong
    answer

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,11 +1,25 @@
 import { useContext } from 'react';
 import { QuizContext } from '../context/quizContext';
 
-const Option = ({ option }) => {
+const Option = ({ option, selectOption, answer }) => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <section className='border-2 border-purple-300 max-w-sm mx-auto my-4 rounded-lg px-4 py-2 mb-2 cursor-pointer hover:bg-purple-400'>
+    <section
+      onClick={() => selectOption()}
+      className={`
+        border-2 border-purple-300 max-w-sm mx-auto my-4 rounded-lg px-4 py-2 mb-2 cursor-pointer hover:bg-purple-400 
+      ${
+        quizState.answerSelected && option === answer
+          ? 'bg-purple-700 hover:bg-purple-700'
+          : ''
+      } ${
+        quizState.answerSelected && option !== answer
+          ? 'bg-red-900 hover:bg-red-900'
+          : ''
+      }
+      `}
+    >
       <p className='text-purple-200'>{option}</p>
     </section>
   );
